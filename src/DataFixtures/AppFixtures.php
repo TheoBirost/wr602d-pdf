0bb7c4bea2952ece0b184dbd642232f68a0e355c
Théo Birost <theo@MacBook-Air-de-Theo.local>
2026-02-09 09:26:52 +0100

    fix: fixture add 4 plans

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -11,8 +11,8 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $plan = new Plan();
-        $plan->setName('FREE');
-        $plan->setDescription('Abonnement gratuit - 2 pdf par jour');
+        $plan->setName('STARTER');
+        $plan->setDescription('Plan gratuit - Idéal pour tester nos services');
         $plan->setPrice(0);
         $plan->setLimitGeneration(2);
         $plan->setActive(true);
@@ -20,19 +20,28 @@ class AppFixtures extends Fixture
         $manager->persist($plan);
 
         $plan = new Plan();
-        $plan->setName('BASIC');
-        $plan->setDescription('Abonnement basic - 20 pdf par jour');
-        $plan->setPrice(9.9);
+        $plan->setName('PRO');
+        $plan->setDescription('Plan Basic - Pour un usage régulier');
+        $plan->setPrice(9);
         $plan->setLimitGeneration(20);
         $plan->setActive(true);
         $plan->setCreatedAt(new \DateTimeImmutable());
         $manager->persist($plan);
 
         $plan = new Plan();
-        $plan->setName('PREMIUM');
-        $plan->setDescription('Abonnement PREMIUM - 200 pdf par jour');
-        $plan->setPrice(45);
+        $plan->setName('ELITE');
+        $plan->setDescription('Plan Premium - Pour les utilisateurs intensifs');
+        $plan->setPrice(29);
         $plan->setLimitGeneration(200);
+        $plan->setActive(true);
+        $plan->setCreatedAt(new \DateTimeImmutable());
+        $manager->persist($plan);
+
+        $plan = new Plan();
+        $plan->setName('LEGEND');
+        $plan->setDescription('Plan Entreprise - Solution complète pour les professionnels');
+        $plan->setPrice(99);
+        $plan->setLimitGeneration(500);
         $plan->setActive(true);
         $plan->setCreatedAt(new \DateTimeImmutable());
         $manager->persist($plan);
