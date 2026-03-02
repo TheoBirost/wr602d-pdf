5cd3183605314bec284ab9118d6807a447a8e2f1
Théo Birost <theo@MacBook-Air-de-Theo.local>
2026-03-02 14:42:28 +0100

    feat : add verif email

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -5,13 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_USER')]
 class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
     public function index(): Response
     {
-        // Logic for user profile management
         return $this->render('profile/index.html.twig');
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,12 +7,14 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -69,10 +71,17 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\JoinColumn(nullable: false)]
     private ?Plan $plan = null;
 
+    #[ORM\Column(type: 'boolean')]
+    private bool $isVerified = false;
+
+    #[ORM\Column(type: Types::INTEGER, options: ['default' => 0])]
+    private int $generationsUsed = 0;
+
     public function __construct()
     {
         $this->userContacts = new ArrayCollection();
         $this->generations = new ArrayCollection();
+        $this->generationsUsed = 0; // Initialize generationsUsed
     }
 
     public function getId(): ?int
@@ -296,6 +305,30 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setPlan(?Plan $plan): static
     {
         $this->plan = $plan;
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): static
+    {
+        $this->isVerified = $isVerified;
+
+        return $this;
+    }
+
+    public function getGenerationsUsed(): int
+    {
+        return $this->generationsUsed;
+    }
+
+    public function setGenerationsUsed(int $generationsUsed): static
+    {
+        $this->generationsUsed = $generationsUsed;
 
         return $this;
     }
